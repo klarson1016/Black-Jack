545388a047c874799d509299f3c7f1c1057eb9d1
Kyler Larson <klarson1016@gmail.com>
2021-07-07 13:32:44 -0700

    Working stay function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
 
 // Declare deck variables
+  const deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
  
-  const suitFace = ['Club', 'Heart', 'Spade', 'Diamond']
-  const cardRank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-  const deck1 = []
-  for (let suitCounter = 0; suitCounter < 4; suitCounter++) {
-    for (let rankCounter = 0; rankCounter < 13; rankCounter++) {
-      deck1.push(cardRank[rankCounter] + suitFace[suitCounter])
-    }
-  }
-
   let deck2 = []
   let dealer=[], player=[], winner
-  let value = []
+  let value = 0
   // Cached element references
   let deck1El = document.getElementById('deck1')
   let deck2El = document.getElementById('deck2')
@@ -45,7 +37,6 @@
         } else {
           dealer.push(cardPicked[0])
         }
-        //let dealerCards = parseInt(dealer)
         console.log("dealer card values", dealer)
         // Pass card picked to render function to display
         getWinner()
@@ -59,30 +50,30 @@
   } 
 
   function getWinner() {
+    value = 0
     let handDeck = turn === 'dealer'? dealer : player
     console.log('deck ', handDeck)
-    let value = 0 
+    //let value = 0 
     let aceNum
     handDeck.forEach(card => {
-      if(card.includes('Jack') || card.includes('Queen') || card.includes('King')) {
+      if(card.includes('J') || card.includes('Q') || card.includes('K')) {
         value += 10
-      } else if (card.includes('Ace')) {
+      } else if (card.includes('A')) {
       aceNum ++
         value += 11
       } else {
-        value += parseInt(card.substring('0', '2'))
+        value += parseInt(card.substring('2', '3'))
       }
     })
-    //console.log('value ', value)
+    console.log('value ', value)
     if (value === 21) {
-      gameStatusDisplayLoc.innerHTML = `Black Jack, ${turn === 'dealer' ? 'Dealer' : 'You' } Wins`
+      gameStatusDisplayLoc.innerHTML = `Black Jack, ${turn === 'player' ? 'dealer' : 'You' } Wins`
     } else if (value > 21){
-      gameStatusDisplayLoc.innerHTML = `Bust! ${turn === 'dealer' ? 'Dealer' : 'You' } Wins`
-    } else if (value > 16) {
-      stayHandle()
-    }
+      gameStatusDisplayLoc.innerHTML = `Bust! ${turn === 'player' ? 'dealer' : 'You' } Wins`
+    } 
 
   }
+
   // Function to render deck state
   function render(cardPicked){
     // //Remove outline class when first card is picked
@@ -125,20 +116,20 @@
       //console.log('Player hand new value ', value)
       getWinner()
   }  
+
   function stayHandle(){
-    // turn = 'dealer'
-    // getWinner()
+    turn = 'dealer'
+    getWinner()
     console.log('Inside stay handle ') 
-    if (value > 16) {  
-        let randIdx = Math.floor(Math.random() * deck1.length) 
+    
+    while (value <= 16){
+     
+      let randIdx = Math.floor(Math.random() * deck1.length) 
         // Assign card with the random index to a variable
         cardPicked = deck1.splice(randIdx, 1)
         // Add card picked to deck 2
         deck2.push(cardPicked[0])
-        if (turn === 'player') {
-          dealer.push(cardPicked[0])
-        } 
-      }
-  
-  
+        dealer.push(cardPicked[0])
+        getWinner()
+    }
 } 
